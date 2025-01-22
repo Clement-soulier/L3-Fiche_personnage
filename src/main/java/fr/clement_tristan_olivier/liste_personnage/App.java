5d20f35cca248be6cd702a9a4bfc7749bed7b5c5
Clément Soulier <clement.soulier12@gmail.com>
2025-01-22 13:42:45 +0100

    initialisation du projet

---
+++ src/main/java/fr/clement_tristan_olivier/liste_personnage/App.java
@@ -0,0 +1,13 @@
+package fr.clement_tristan_olivier.liste_personnage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
